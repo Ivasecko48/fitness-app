7c13a2dc049f41bea54d1a969d4ad85e57616681
ivasecko48 <robertivasecko2001@gmail.com>
2025-01-20 04:09:37 +0100

    added functionality

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // meal class
 class Meal {
   constructor(time, comps, kcal) {
-    this.date = Date;
+    this.id = Date.now();
+    let d = new Date();
+    this.date = Intl.DateTimeFormat('en-GB').format(d);
     this.time = time;
     this.comps = comps;
     this.kcal = kcal;
@@ -13,28 +15,7 @@ class Meal {
 // UI class
 class UI {
   static displayMeals() {
-    const storedMeals = [
-      {
-        date: '20/01/2025',
-        time: 2,
-        comps: 'banana',
-        kcal: 100,
-      },
-      {
-        date: '19/01/2025',
-        time: 2,
-        comps: 'banana',
-        kcal: 100,
-      },
-      {
-        date: '21/01/2025',
-        time: 2,
-        comps: 'banana',
-        kcal: 100,
-      },
-    ];
-
-    const meals = storedMeals;
+    const meals = Store.getMeals();
 
     meals.forEach((meal) => UI.addMealToList(meal));
   }
@@ -43,6 +24,7 @@ class UI {
     const list = document.querySelector('#meal-list');
     const row = document.createElement('tr');
 
+    row.setAttribute('data-id', meal.id);
     row.innerHTML = `
         <td>${meal.time}</td>
         <td>${meal.comps}</td>
@@ -52,10 +34,63 @@ class UI {
 
     list.appendChild(row);
   }
+  static deleteMeal(el) {
+    if (el.classList.contains('delete')) {
+      el.parentElement.parentElement.remove();
+      UI.showAlert('NIGGA', 'success');
+    }
+  }
+
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#meal-form');
+    container.insertBefore(div, form);
+    //vanish in spec time
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  }
+
+  static clearFields() {
+    document.querySelector('#meal-time').value = '';
+    document.querySelector('#meal-components').value = '';
+    document.querySelector('#meal-calories').value = '';
+  }
 }
 
 //store class
+class Store {
+  static getMeals() {
+    let meals;
+    if (localStorage.getItem('meals') === null) {
+      meals = [];
+    } else {
+      meals = JSON.parse(localStorage.getItem('meals'));
+    }
+    return meals;
+  }
 
+  static addMeal(meal) {
+    const meals = Store.getMeals();
+
+    meals.push(meal);
+
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+
+  static removeMeal(id) {
+    const meals = Store.getMeals();
+
+    meals.forEach((meal, index) => {
+      if (meal.id == id) {
+        meals.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('meals', JSON.stringify(meals));
+  }
+}
 //event: display
 document.addEventListener('DOMContentLoaded', UI.displayMeals);
 
@@ -69,7 +104,23 @@ document.querySelector('#meal-form').addEventListener('submit', (e) => {
 
   //instantiate
   const meal = new Meal(time, comps, kcals);
-
-  console.log(meal);
+  // add meal to UI
+  UI.addMealToList(meal);
+  //add meal to store
+  Store.addMeal(meal);
+  //show nigga msg
+  UI.showAlert('NIGGA', 'success');
+  //clear fields
+  UI.clearFields();
 });
 //event:REMOVE
+document.querySelector('#meal-list').addEventListener('click', (e) => {
+  UI.deleteMeal(e.target); //from UI
+  let id = e.target.parentElement.parentElement.getAttribute('data-id');
+  Store.removeMeal(id); //from store
+});
+//tasks: background picture for meals and training
+//navbar
+// sort by date
+//total kcal and volume
+//exercise tab (show,hide)
